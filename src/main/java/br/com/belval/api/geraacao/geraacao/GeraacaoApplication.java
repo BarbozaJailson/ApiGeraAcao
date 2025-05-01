c5e56fb49dd01be07061c855fb5b3fd5a89839f9
BarbozaJailson <jailson.100vicio@hotmail.com>
2025-04-30 22:42:56 -0300

    Versão inicial

---
+++ src/main/java/br/com/belval/api/geraacao/geraacao/GeraacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.belval.api.geraacao.geraacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeraacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeraacaoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/belval/api/geraacao/geraacao/model/Doacao.java
@@ -0,0 +1,13 @@
+package br.com.belval.api.geraacao.geraacao.model;
+
+import java.time.LocalDate;
+
+public class Doacao {
+	private Long id;
+	private LocalDate data;
+	private int quantidade;
+	private String staus;
+
+	
+
+}
